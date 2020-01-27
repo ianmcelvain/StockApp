ba13a42136ae6e9dd5ef1677142c487dbb4485f0
VirtualCucumber <45335671+VirtualCucumber@users.noreply.github.com>
2020-01-27 09:47:19 -0600

    Updates

---
+++ precache-manifest.96fe796b447720891e7414f38fe0cbf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03223f4584b6a286771160b7208f2d0f",
+    "url": "/StockApp/index.html"
+  },
+  {
+    "revision": "2eadf7c0c030b0904977",
+    "url": "/StockApp/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "60c95be3766284742374",
+    "url": "/StockApp/static/css/main.194ab6ce.chunk.css"
+  },
+  {
+    "revision": "2eadf7c0c030b0904977",
+    "url": "/StockApp/static/js/2.3aebe037.chunk.js"
+  },
+  {
+    "revision": "823e07f15d08def7b72d7bd05c387f60",
+    "url": "/StockApp/static/js/2.3aebe037.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60c95be3766284742374",
+    "url": "/StockApp/static/js/main.b98b8d2d.chunk.js"
+  },
+  {
+    "revision": "29d6114c2ae12e575ef4",
+    "url": "/StockApp/static/js/runtime-main.769bb056.js"
+  }
+]);
